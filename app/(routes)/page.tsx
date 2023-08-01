7d31c1ffcc2f4de630255c6845c98261273bd36f
Raphael Faria <raphaelf9@outlook.com>
2023-08-01 13:13:04 -0300

    Config do ambiente a adição de produtos

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0; //cache!
+
+const HomePage = async () => {
+
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("c9bcdcc4-adc8-4446-abfc-0f563ffe2348");
+  return (
+    <>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Featured Products" items={products} />
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+};
+
+export default HomePage;
---
+++ components/ui/currency.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from 'react'
+
+export const formatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL"
+})
+
+interface CurrencyProps {
+  value?: string | number
+}
+
+const Currency: React.FC<CurrencyProps> = ({
+  value
+}) => {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="font-semibold">
+      {formatter.format(Number(value))}
+    </div>
+  );
+};
+
+
+export default Currency;
