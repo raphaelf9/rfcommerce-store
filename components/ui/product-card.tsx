d208aacd4052ade66c1fc6f68b01ba57c9d35284
Raphael Faria <raphaelf9@outlook.com>
2023-08-01 16:00:35 -0300

    Tela de Produto-Individual

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: Product;
@@ -14,8 +15,13 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
+
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`)
+  }
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/*Imagens e ações dos produtos */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image alt="Imagem" src={data?.images?.[0].url} fill className="aspect-square object-cover rounded-md" />
