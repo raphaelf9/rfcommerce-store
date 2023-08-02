8da9772f4f2e4dad8046800098bb7bb277d4a22e
Raphael Faria <raphaelf9@outlook.com>
2023-08-02 16:29:18 -0300

    Modal do componente de produto

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,6 +6,8 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
@@ -16,34 +18,41 @@ const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
 
+  const previewModal = usePreviewModal();
+
   const router = useRouter();
+
   const handleClick = () => {
     router.push(`/product/${data?.id}`)
   }
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  }
+
   return (
-    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/*Imagens e ações dos produtos */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image alt="Imagem" src={data?.images?.[0].url} fill className="aspect-square object-cover rounded-md" />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton onClick={() => { }} icon={<Expand size={20} className="text-gray-600" />} />
+            <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600" />} />
             <IconButton onClick={() => { }} icon={<ShoppingCart size={20} className="text-gray-600" />} />
           </div>
         </div>
       </div>
       {/*Descrição do produto */}
       <div>
-        <p className="font-semibold text-lg">
-          {data.name}
-        </p>
-        <p className="text-sm text-gray-500">
-          {data.category?.name}
-        </p>
+        <p className="font-semibold text-lg"> {data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
       </div>
       {/*Preço do produto */}
-      <div className="flex-items-center justify-between">
-        <Currency value={data?.price} />
+      <div className="flex items-center justify-between"><Currency value={data?.price} />
       </div>
     </div>
   )
