93fe43c1e702667e08a57be10447ba0a9b7ba66d
Raphael Faria <raphaelf9@outlook.com>
2023-08-03 12:19:04 -0300

    Correção de erro de Hydration no carrinho

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,10 +15,11 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
 }, ref) => {
   return (
     <button
-      ref={ref}
-      {...props}
       className
-      ={cn(`w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`, className)}>
+      ={cn(`w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`, className)}
+      disabled={disabled}
+      ref={ref}
+      {...props}>
       {children}
     </button>
   )
